4ee607d7ed5c49e960962ffb2df1553efd3ad723
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-25 01:29:14 -0400

    6 out of 20

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
 console.log('The Iron Yard Rocks');
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 210) {
+                    $("body").css('background-color', 'white');
+                } else {
+                    $("body").css('background-color', 'red');
+                }
+            });
+        });
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 600) {
+                    $(".menu").css('background-color', 'white');
+                    $('.menu').css('opacity', 0.95);
+                } else {
+                    $(".menu").css('background-color','black');
+                }
+            });
+        });
+
+
+    jQuery(document).ready(function() {
+	jQuery('.box').hide();
+});
+jQuery(document).ready(function() {
+	jQuery('.container').hover(function() {
+		jQuery('.box').show();
+		},
+	function() {
+		jQuery('.box').hide();
+		});
+});
