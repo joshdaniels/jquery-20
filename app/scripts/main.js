3c1193e11bf5785788690c70973e81d92dec905f
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-25 03:44:09 -0400

    14 out of 20

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,10 @@
 console.log('The Iron Yard Rocks');
+
 $(document).ready(function(){       
             var scroll_pos = 0;
             $(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 210) {
-                    $("body").css('background-color', 'white');
-                } else {
-                    $("body").css('background-color', 'red');
-                }
-            });
-        });
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 600) {
+                if(scroll_pos > 300) {
                     $(".menu").css('background-color', 'white');
                     $('.menu').css('opacity', 0.95);
                 } else {
@@ -34,4 +24,30 @@ jQuery(document).ready(function() {
 	function() {
 		jQuery('.box').hide();
 		});
+});
+
+$(".button").click(function(){
+  $(".movies-hover").hide(".movies-hover");
+  $(".movies-hover2").show(".movies-hover2");
+  $(".movies-hover2").addClass(".movies-hover3");
+});
+
+$(document).ready(function(){
+  $(".movies-hover").find(".container").css({"color":"red","border":"2px solid red"});
+});
+
+$(".button").click(function(){
+  alert($(".container").width());
+}); 
+
+$(".button").click(function(){
+  alert($(".container").height());
+});
+
+$("p").one("click",function(){
+  $(this).animate({fontSize:"+=30px"});
+});
+
+$("body").keydown(function(){
+  $("body").css("background-color","red");
 });
